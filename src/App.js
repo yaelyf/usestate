13b6d2144e160cce05f8b9b93f8fbec94677f64e
Yael Yaffe <yaelyf@post.jce.ac.il>
2022-12-06 12:06:11 +0200

    Added maximum counter value for all counters

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import { useState } from 'react';
 
 function App() {
   const [delta, setDelta] = useState(1)
-  const [maxNum, setMaxNum] = useState(1)
+  const [maxNum, setMaxNum] = useState(100)
   const [reset, setReset] = useState(false)
+  const [maxValue, changeMaxValue] = useState(0)
 
   function handleDelta(e){
-    console.log(e);
     setDelta(Number(e.target.value))
   }
   function handleMaximum(e){
@@ -21,17 +21,22 @@ function App() {
     setReset(data)
   }
 
+  function setMaxValue(counterValue){
+    if (counterValue > maxValue) changeMaxValue(counterValue)
+  }
+
   return (
     <div className="App">
       <h1>Hello to advanced internet technologies class</h1>
       <p></p>
+      <h3>Maximum counters value {maxValue}</h3>
       <h5>Delta counter:</h5>
       <input type = "number" value = {delta} onChange = {handleDelta} />
       <p></p>
       <h5>Maximum counter number:</h5>
       <input type = "number" value = {maxNum} onChange = {handleMaximum} />
-      <Counter delta = {delta} maxNum = {maxNum} getReset = {getReset} needToReset = {reset}/>
-      <Counter delta = {delta} maxNum = {maxNum} getReset = {getReset} needToReset = {reset}/>
+      <Counter delta = {delta} maxNum = {maxNum} getReset = {getReset} needToReset = {reset} setMaxValue = {setMaxValue}/>
+      <Counter delta = {delta} maxNum = {maxNum} getReset = {getReset} needToReset = {reset} setMaxValue = {setMaxValue}/>
     </div>
   );
 }
